810a85636fb3cb90beccc800e25fa4af9fc486e3
Stanisław Głogowski <stanislaw.glogowski@gmail.com>
2019-10-23 20:58:24 +0200

    Updates contract methods

--- packages/eth-sdk-contract/src/interfaces.ts
+++ packages/eth-sdk-contract/src/interfaces.ts
@@ -1,14 +1,17 @@
+import { Subject } from 'rxjs';
 import { queryModules } from '@eth-sdk/query';
+import { TTagOrQuantity } from '@eth-sdk/utils';
 
-export interface IContract<F = null, E = null> {
+export interface IContract<F = null, E = null, K = E extends null ? string : keyof E> {
   address: string;
+  allEvents: E extends null ? IContract.IEvent : IContract.IEvent<K>;
   events: E extends null ? {
-    [key: string]: IContract.IEvent;
+    [key: string]: IContract.IEvent & IContract.ISignature;
   } : {
-    [K in keyof E]: IContract.IEvent<E[K]>;
+    [K in keyof E]: IContract.IEvent<K, E[K]> & IContract.ISignature;
   };
   methods: F extends null ? {
-    [key: string]: IContract.IMethod;
+    [key: string]: IContract.TMethod & IContract.ISignature;
   } : {
     [K in keyof F]: F[K] & IContract.ISignature;
   };
@@ -19,18 +22,23 @@ export namespace IContract {
     signature: string;
   }
 
-  export interface IEvent<E = string, P = any> extends ISignature {
-    getLogs(): Promise<IEventLog<E, P>[]>;
+  export interface IEvent<E = string, P = any> {
+    getLogs(options?: IEventGetLogsOptions): Promise<IEventLog<E, P>[]>;
+
+    subscribeLogs(): Promise<Subject<IEventLog<E, P>>>;
   }
 
-  export interface IEventLog<E = string, P = any> {
+  export interface IEventLog<E = string, P = any> extends queryModules.Eth.ILogResult {
     event: E;
     payload: P;
   }
 
-  export interface IMethod extends ISignature {
-    (...args: any[]): IMethodExecute;
+  export interface IEventGetLogsOptions {
+    fromBlock?: TTagOrQuantity;
+    toBlock?: TTagOrQuantity;
   }
+
+  export type TMethod = (...args: any[]) => IMethodExecute;
 
   export interface IMethodExecute<T = any> {
     data: string;
