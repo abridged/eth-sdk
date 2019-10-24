428c06bfef85df3288b08d968d762d6e340f28da
Stanisław Głogowski <stanislaw.glogowski@gmail.com>
2019-10-24 13:43:04 +0200

    Updates examples

---
+++ examples/node-key-http-provider/main.js
@@ -0,0 +1,34 @@
+const { Query, Key, queryProviders, randomAddress } = require('eth-sdk');
+const fetch = require('node-fetch');
+
+// see: https://github.com/etherspot/etherspot-local
+const HTTP_PROVIDER_ENDPOINT = 'http://localhost:8545';
+const PRIVATE_KEY = '0x9e73ef82d7e4ebeb1d3ae220df7ccd0db239c65092fc0f42084db75af7f10e3a';
+
+async function main() {
+  const provider = new queryProviders.HttpProvider(HTTP_PROVIDER_ENDPOINT, {
+    fetch,
+  });
+
+  const query = new Query(provider);
+  const key = new Key(PRIVATE_KEY, query);
+
+  query
+    .currentProvider
+    .addExtension(key);
+
+  const to = randomAddress();
+  const value = 999;
+
+  const hash = await query.eth.sendTransaction({
+    from: await key.address,
+    to,
+    value,
+  });
+
+  console.log('hash:', hash);
+}
+
+main()
+  .catch(console.log);
+
