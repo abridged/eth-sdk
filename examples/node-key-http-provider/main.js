1e1c46e24d3072eebe9f7930684d2bdb4bd56588
Stanisław Głogowski <stanislaw.glogowski@gmail.com>
2019-11-19 09:24:09 +0100

    Updates examples

--- examples/node-key-http-provider/main.js
+++ examples/node-key-http-provider/main.js
@@ -31,4 +31,3 @@ async function main() {
 
 main()
   .catch(console.log);
-
--- examples/node-query-http-provider/main.js
+++ examples/node-query-http-provider/main.js
@@ -3,15 +3,15 @@ const fetch = require('node-fetch');
 
 async function main() {
   const provider = new queryProviders
-    .HttpProvider('https://mainnet.infura.io/', {
+    .HttpProvider('https://kovan.infura.io/', {
       fetch,
     });
 
   const query = new Query(provider);
 
   console.log('query.net.version:', await query.net.version);
+  console.log('query.eth.chainId:', await query.eth.chainId);
 }
 
 main()
   .catch(console.log);
-
